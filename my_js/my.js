38995f2b500e9765ac26bc682c3f7c16d2db7360
romanb123 <44100462+romanb123@users.noreply.github.com>
2019-04-19 22:24:59 +0300

    dsd

--- my_js/my.js
+++ my_js/my.js
@@ -5,17 +5,22 @@ $(document).ready(function () {
                 console.log(result);
                 for (var i = 0, len = 10; i < len; i++) {
                     $(".row").append(`
-<div class="col-lg-3">
 <!-- card -->
-<div class="card">
+<div class="card col-lg-3">
     <div class="card-body">
-        <h4 class="card-title">${result[i].id}</h4>
-        <h4 class="card-title">${result[i].symbol}</h4>
-        <h4 class="card-title">${result[i].name}</h4>
-        <a href="#" class="btn btn-primary">See Profile</a>
+        <h4 class="card-title"><span class="bold">id=</span>"${result[i].id}"</h4>
+        <h4 class="card-title"><span class="bold">symbol"</span>=
+        ${result[i].symbol}"</h4>
+        <h4 class="card-title"><span class="bold">"name=</span>${result[i].name}"</h4>
+        <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo${i}">Simple collapsible</button>
+        <div id="demo${i}" class="collapse in">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </div>
     </div>
 </div>
-</div>
+
 `);
                 }
 
