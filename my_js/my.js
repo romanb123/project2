4dce7c0c0ff10110a9538222d39571df355f8089
romanb123 <44100462+romanb123@users.noreply.github.com>
2019-04-20 19:15:09 +0300

    upfd

--- my_js/my.js
+++ my_js/my.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-// ========================
-// function for all coins
-// ========================
+    // ========================
+    // function for all coins
+    // ========================
     $(".allcoins").click(function () {
         $.ajax({
             url: "https://api.coingecko.com/api/v3/coins/list", success: function (result) {
@@ -12,13 +12,13 @@ $(document).ready(function () {
 <div class="card col-lg-3">
     <div class="card-body">  
     <h1 class="card-title">${result[i].name}</h1>
-        <h4 class="card-title"><span class="bold">id=</span>"${result[i].id}"</h4>
+        <h4 class="card-title"><span class="bold">id=</span>"<span id="identify">${result[i].id}</span>"</h4>
         <h4 class="card-title"><span class="bold">symbol"</span>=
         ${result[i].symbol}"</h4>
       
         <button type="button" class="btn btn-info moreinfo" data-toggle="collapse" data-target="#demo${i}">Simple collapsible</button>
         <div id="demo${i}" class="collapse in">
-         <h1>${result[i].id}</h1>
+        
         </div>
     </div>
 </div>
@@ -28,15 +28,19 @@ $(document).ready(function () {
             }
         });
     });
-// ========================
-// function fo more info
-// ========================
-$("body").on('click','.moreinfo',function(){
-var coinid=$(this).parent().find("h1:last").text();
-console.log(coinid);
-    $.ajax({url: `https://api.coingecko.com/api/v3/coins/`+coinid, success: function(result){
-      $(body).find(".moreinfo").html("df");
-    }});
-      });
-
+    // ========================
+    // function fo more info
+    // ========================
+    $("body").on('click', '.moreinfo', function () {
+        var coinid = $(this).parent().find("#identify").text();
+        var dataplace=$(this).attr("data-target");
+        console.log(coinid);
+        console.log(dataplace);
+        $.ajax({
+            url: `https://api.coingecko.com/api/v3/coins/` + coinid, success: function (result) {
+                $("body").find(`${dataplace}`).html(JSON.stringify(result));
+                console.log(result);
+            }
+        });
+    });
 });
