29e6f60ef82e9f7ae19e190f8436e6efe845db71
romanb123 <44100462+romanb123@users.noreply.github.com>
2019-04-20 14:22:54 +0300

    upd

--- my_js/my.js
+++ my_js/my.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-    $("button").click(function () {
+// ========================
+// function for all coins
+// ========================
+    $(".allcoins").click(function () {
         $.ajax({
             url: "https://api.coingecko.com/api/v3/coins/list", success: function (result) {
                 console.log(result);
@@ -7,25 +10,33 @@ $(document).ready(function () {
                     $(".row").append(`
 <!-- card -->
 <div class="card col-lg-3">
-    <div class="card-body">
+    <div class="card-body">  
+    <h1 class="card-title">${result[i].name}</h1>
         <h4 class="card-title"><span class="bold">id=</span>"${result[i].id}"</h4>
         <h4 class="card-title"><span class="bold">symbol"</span>=
         ${result[i].symbol}"</h4>
-        <h4 class="card-title"><span class="bold">"name=</span>${result[i].name}"</h4>
-        <button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo${i}">Simple collapsible</button>
+      
+        <button type="button" class="btn btn-info moreinfo" data-toggle="collapse" data-target="#demo${i}">Simple collapsible</button>
         <div id="demo${i}" class="collapse in">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+         <h1>${result[i].id}</h1>
         </div>
     </div>
 </div>
 
 `);
                 }
-
-
             }
         });
     });
+// ========================
+// function fo more info
+// ========================
+$("body").on('click','.moreinfo',function(){
+var coinid=$(this).parent().find("h1:last").text();
+console.log(coinid);
+    $.ajax({url: `https://api.coingecko.com/api/v3/coins/`+coinid, success: function(result){
+      $(body).find(".moreinfo").html("df");
+    }});
+      });
+
 });
